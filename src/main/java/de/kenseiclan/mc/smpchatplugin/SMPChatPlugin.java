ca8028519a6ebf6f3ab36dba669dbf682854a355
Nevah5 <neevaah5@gmail.com>
2024-02-04 01:28:42 +0100

    Initial Commit

---
+++ src/main/java/de/kenseiclan/mc/smpchatplugin/SMPChatPlugin.java
@@ -0,0 +1,17 @@
+package de.kenseiclan.mc.smpchatplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMPChatPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
