91a4145fd83181befdec15de50d8db6063b3980d
Nevah5 <neevaah5@gmail.com>
2024-02-04 02:35:09 +0100

    Added config helper and default config

--- src/main/java/de/kenseiclan/mc/smpchatplugin/SMPChatPlugin.java
+++ src/main/java/de/kenseiclan/mc/smpchatplugin/SMPChatPlugin.java
@@ -4,9 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SMPChatPlugin extends JavaPlugin {
 
+    @Getter
+    private ConfigHelper configHelper;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        this.saveDefaultConfig();
+        configHelper = new ConfigHelperImpl(this);
 
     }
 
