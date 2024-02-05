cf4abbf012a9e70ad1998a2f54481f99cb197d41
Nevah5 <neevaah5@gmail.com>
2024-02-05 01:03:36 +0100

    Added Menu Header String

--- src/main/java/de/kenseiclan/mc/smpchatplugin/commands/ChatStrings.kt
+++ src/main/java/de/kenseiclan/mc/smpchatplugin/commands/ChatStrings.kt
@@ -4,6 +4,8 @@ class ChatStrings {
     companion object {
         private const val PLUGIN_CHAT_PREFIX = "<gradient:#41c9b0:#287c6d><bold>SMP-CHAT</bold></gradient>"
         const val CHAT_MESSAGE = "$PLUGIN_CHAT_PREFIX <dark_gray>|<dark_gray> <gray><message>"
-        const val NO_PERMISSION = "$PLUGIN_CHAT_PREFIX <dark_gray>|<dark_gray> <gray>You are unauthorized to do this."
+        const val NO_PERMISSION = "$PLUGIN_CHAT_PREFIX <dark_gray>|<dark_gray> <red>You are unauthorized to do this."
+        const val MENU_HEADER =
+            "$PLUGIN_CHAT_PREFIX <dark_gray>|<dark_gray> <gradient:#41c9b0:#287c6d><bold>-------</bold> <green><title></green> <bold>-------</bold>"
     }
 }
