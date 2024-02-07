bfb9f92cfd46505befe62375862860fb1f58e521
Nevah5 <neevaah5@gmail.com>
2024-02-07 01:14:50 +0100

    Player Tablist format

--- src/main/java/de/kenseiclan/mc/smpchatplugin/events/JoinEventHandler.kt
+++ src/main/java/de/kenseiclan/mc/smpchatplugin/events/JoinEventHandler.kt
@@ -1,9 +1,9 @@
 package de.kenseiclan.mc.smpchatplugin.events;
 
 import de.kenseiclan.mc.smpchatplugin.SMPChatPlugin
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import net.luckperms.api.model.group.Group
 import net.luckperms.api.model.user.User
-import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -17,14 +17,16 @@ class JoinEventHandler(private val plugin: SMPChatPlugin) : Listener {
         val group = plugin.luckPerms.provider.groupManager.getGroup(user.primaryGroup)
 
         val playerTabListName =
-            ChatColor.translateAlternateColorCodes('&', getFormattedPlayerName(user, group, joinEvent.player.name))
-        joinEvent.player.setPlayerListName(playerTabListName)
+            SMPChatPlugin.miniMessage.deserialize(getFormattedPlayerName(user, group, joinEvent.player.name))
+
+        val legacyText = LegacyComponentSerializer.legacySection().serialize(playerTabListName)
+        joinEvent.player.setPlayerListName(legacyText)
     }
 
     private fun getFormattedPlayerName(user: User, group: Group?, playerName: String): String {
         var groupPrefix = plugin.chatGroupProvider.getPlayerChatGroup(user, group).prefix
         if (groupPrefix.isNotEmpty()) groupPrefix += " "
-        val userPrefix = user.cachedData.metaData.prefix ?: "&7"
-        return "$groupPrefix&r$userPrefix$playerName"
+        val userPrefix = user.cachedData.metaData.prefix ?: "<gray>"
+        return "$groupPrefix<reset>$userPrefix$playerName<reset>"
     }
 }
