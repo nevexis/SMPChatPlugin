408b49248309b98206aae40f0a7bc33ff3eab3f4
Nevah5 <neevaah5@gmail.com>
2024-02-07 01:07:42 +0100

    Using adventure for chat

--- src/main/java/de/kenseiclan/mc/smpchatplugin/events/ChatEventHandler.kt
+++ src/main/java/de/kenseiclan/mc/smpchatplugin/events/ChatEventHandler.kt
@@ -3,12 +3,12 @@ package de.kenseiclan.mc.smpchatplugin.events;
 import de.kenseiclan.mc.smpchatplugin.SMPChatPlugin
 import net.luckperms.api.model.group.Group
 import net.luckperms.api.model.user.User
-import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
 import org.bukkit.event.player.PlayerCommandPreprocessEvent
+
 
 class ChatEventHandler(private val plugin: SMPChatPlugin) : Listener {
     private val chatMessageSymbol: String = plugin.config.getString("chat.chat-symbol") ?: "»"
@@ -21,10 +21,12 @@ class ChatEventHandler(private val plugin: SMPChatPlugin) : Listener {
         requireNotNull(user)
         val group = plugin.luckPerms.provider.groupManager.getGroup(user.primaryGroup)
 
-        val message = getFormattedChatMessage(chatEvent.player, chatEvent.message, user, group)
+        var message = getFormattedChatMessage(chatEvent.player, chatEvent.message, user, group)
 
-        chatEvent.recipients.forEach { player ->
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', message))
+        chatEvent.recipients.forEach { _ ->
+            val audience = SMPChatPlugin.adventure.players()
+            val adventureFormattedMessage = SMPChatPlugin.miniMessage.deserialize(message)
+            audience.sendMessage(adventureFormattedMessage)
         }
     }
 
@@ -42,14 +44,17 @@ class ChatEventHandler(private val plugin: SMPChatPlugin) : Listener {
         formattedMessage = getFormattedChatMessage(event.player, formattedMessage, user, group)
         formattedMessage = "$chatMessageSymbol $formattedMessage"
 
-        plugin.server.broadcastMessage(ChatColor.translateAlternateColorCodes('&', formattedMessage))
+        val audience = SMPChatPlugin.adventure.players()
+        val adventureFormattedMessage = SMPChatPlugin.miniMessage.deserialize(formattedMessage)
+        audience.sendMessage(adventureFormattedMessage)
     }
 
     private fun getFormattedChatMessage(player: Player, message: String, user: User, group: Group?): String {
         var groupPrefix = plugin.chatGroupProvider.getPlayerChatGroup(user, group).prefix
         if (groupPrefix.isNotEmpty()) groupPrefix += " "
-        val userPrefix = user.cachedData.metaData.prefix ?: "&7"
+        val userPrefix = user.cachedData.metaData.prefix ?: "<gray>"
         val playerName = player.name
-        return "$groupPrefix&r$userPrefix$playerName &8$chatMessageSymbol &r$message"
+
+        return "$groupPrefix$userPrefix$playerName<reset> <dark_gray>$chatMessageSymbol</dark_gray> <gray>$message"
     }
 }
